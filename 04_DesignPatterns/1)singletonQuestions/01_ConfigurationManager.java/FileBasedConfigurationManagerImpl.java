43f727e25226a00dc70b86a6644131004f9978cb
HimanshuRanjan <livelikehimanshu12@gmail.com>
2025-01-12 19:48:42 +0530

    Added coding question for Singleton Configuration manager

---
+++ 04_DesignPatterns/1)singletonQuestions/01_ConfigurationManager.java/FileBasedConfigurationManagerImpl.java
@@ -0,0 +1,62 @@
+package com.assignment.question;
+
+public class FileBasedConfigurationManagerImpl extends FileBasedConfigurationManager {
+    private static FileBasedConfigurationManagerImpl instance;
+
+    //making consturctor private
+    private FileBasedConfigurationManagerImpl(){
+
+    }
+
+    public static FileBasedConfigurationManager getInstance() {
+        if(instance == null) {
+            instance = new FileBasedConfigurationManagerImpl();
+        }
+        return instance;
+    }
+
+    public static void resetInstance() {
+        instance = null; 
+    }
+
+    @Override
+    public String getConfiguration(String key) {
+        return properties.getProperty(key);
+        //throw new UnsupportedOperationException("Unimplemented method 'getConfiguration'");
+    }
+
+    @Override
+    public <T> T getConfiguration(String key, Class<T> type) {
+        String value = properties.getProperty(key);
+        if(value!=null) {
+            return instance.convert(value, type);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public void setConfiguration(String key, String value) {
+        properties.setProperty(key, value);
+        //throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+    }
+
+    @Override
+    public <T> void setConfiguration(String key, T value) {
+         properties.setProperty(key, value.toString());
+        //throw new UnsupportedOperationException("Unimplemented method 'setConfiguration'");
+    }
+
+    @Override
+    public void removeConfiguration(String key) {
+        properties.remove(key);
+        //throw new UnsupportedOperationException("Unimplemented method 'removeConfiguration'");
+    }
+
+    @Override
+    public void clear() {
+        properties.clear();
+        //throw new UnsupportedOperationException("Unimplemented method 'clear'");
+    }
+
+}
