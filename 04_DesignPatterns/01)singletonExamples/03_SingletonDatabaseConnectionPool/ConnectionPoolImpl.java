8394bae92af546215623631a01dfd3356081c479
HimanshuRanjan <livelikehimanshu12@gmail.com>
2025-02-05 18:46:22 +0530

    Changed data structure t store connectionpools from ArrayList tro Hashmap

--- 04_DesignPatterns/01)singletonExamples/03_SingletonDatabaseConnectionPool/ConnectionPoolImpl.java
+++ 04_DesignPatterns/01)singletonExamples/03_SingletonDatabaseConnectionPool/ConnectionPoolImpl.java
@@ -1,27 +1,35 @@
 package com.assignment.question;
 
-import java.util.*;
+import java.util.HashMap;
 
 public class ConnectionPoolImpl implements ConnectionPool {
     private static ConnectionPoolImpl instance;
-    private int poolSize;
-
-    /*
+    
+     /*
      * In my network pool, lets say I have 10 connections. For each connection, 
      * I need to identify this connection as well as mark whether its available or not
      * so, I will use a class with 2 field, id and isAvailable 
+     * 
+     * Further, to save list of all connections, I have 2 options
+     * 1. Store in arraylist
+     * 2. Store in hashmap,
+     * 
+     * Clearly, hashmap is more time effieicnt, so using it
      */
-    private ArrayList<DatabaseConnection> pool;//to store connection
-
-    //step-1 Make constructor private
-    private ConnectionPoolImpl(int poolSize) {
-        this.poolSize = poolSize;
-        this.pool = new ArrayList<DatabaseConnection>(); //without this, I was getting error!!!. My bad
+    private int poolSize;
+    private HashMap<Integer, DatabaseConnection> connectionPool = new HashMap<Integer, DatabaseConnection>();
+    
+    //step-1: Making contructor private
+    private ConnectionPoolImpl(int maxConnections) {
+        this.poolSize = maxConnections;
         initializePool();
     }
-
-    //step-2 create a static method to get the singeton instance
+    
+    //step-2: Create static getInstance
     public static ConnectionPoolImpl getInstance(int maxConnections) {
+        /*Even without using synchronised, simple variant was also passing all test cases
+          But its best to write code that supports multithreaded env  
+        */
         if(instance==null) {
             synchronized(ConnectionPoolImpl.class) {
                 if(instance == null) {
@@ -38,23 +46,25 @@ public class ConnectionPoolImpl implements ConnectionPool {
 
     @Override
     public void initializePool() {
+        //Connection n number of pools. they are avaiable for use
         for(int i=0; i<this.poolSize; i++) {
-            pool.add(new DatabaseConnection(i, true));
+            this.connectionPool.put(i, new DatabaseConnection(i, true));
         }
     }
 
     @Override
     public DatabaseConnection getConnection() {
-        if(this.pool == null){
+        //Even without this check, the code was working and test cases were passing
+        //but its good pracrice to check for exceptions 
+        if(this.connectionPool == null){
             throw new NullPointerException();
         }
         for(int i=0; i<this.poolSize; i++) {
-            DatabaseConnection curr = this.pool.get(i);
-            if(curr.isAvailable) {
-                curr.isAvailable = false;
-                return curr;
-                //this.pool.set(i, new DatabaseConnection(i, false));
-                //return this.pool.get(i);
+            DatabaseConnection con = this.connectionPool.get(i);
+            if(con.isAvailable) {
+                con.isAvailable = false;
+                this.connectionPool.put(i, con);
+                return con;
             }
         }
         return null;
@@ -62,27 +72,22 @@ public class ConnectionPoolImpl implements ConnectionPool {
 
     @Override
     public void releaseConnection(DatabaseConnection connection) {
-        if(this.pool == null){
+        if(this.connectionPool == null){
             throw new NullPointerException();
         }
-        for(int i=0; i<this.poolSize; i++) {
-            DatabaseConnection curr = this.pool.get(i);
-            if(curr.id == connection.id) {
-                curr.isAvailable = true;
-                //this.pool.set(i, new DatabaseConnection(i, true));
-            }
-        }
-
+        int id = connection.id;
+        connection.isAvailable = true;
+        this.connectionPool.put(id, connection); 
     }
 
     @Override
     public int getAvailableConnectionsCount() {
-        if(this.pool == null){
+        if(this.connectionPool == null){
             throw new NullPointerException();
         }
         int count = 0;
         for(int i=0; i<this.poolSize; i++) {
-            if(this.pool.get(i).isAvailable) {
+            if(this.connectionPool.get(i).isAvailable) {
                 count++;
             }
         }
@@ -91,7 +96,7 @@ public class ConnectionPoolImpl implements ConnectionPool {
 
     @Override
     public int getTotalConnectionsCount() {
-        if(this.pool == null){
+        if(this.connectionPool == null){
             throw new NullPointerException();
         }
         return this.poolSize;
